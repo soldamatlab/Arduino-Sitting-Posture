d0dd32f7660f573cff3140b3097e053c8951de15
soldamatlab <matoussoldat@email.cz>
2022-11-26 19:54:59 +0100

    Change function bracket formatting

--- two_sens/two_sens.ino
+++ two_sens/two_sens.ino
@@ -5,39 +5,35 @@
 
 float acceleration[N_SENSORS][N_SENSOR_VALUES];
 
-void setup()
-{
-  initSens();
+void setup() {
+    initSens();
 
-  delay(20);
+    delay(20);
 }
 
-void loop()
-{
-  readSens(0, acceleration[0]);
-  readSens(1, acceleration[1]);
+void loop() {
+    readSens(0, acceleration[0]);
+    readSens(1, acceleration[1]);
 
-  printSensValues(0);
-  printSensValues(1);
-  Serial.println();
+    printSensValues(0);
+    printSensValues(1);
+    Serial.println();
 
-  delay(250);
+    delay(250);
 }
 
-void printSensValues(int MPU_idx)
-{
-  printSensIdx("ax", MPU_idx);
-  Serial.print(acceleration[MPU_idx][0]);
-  printSensIdx("ay", MPU_idx);
-  Serial.print(acceleration[MPU_idx][1]);
-  printSensIdx("ax", MPU_idx);
-  Serial.print(acceleration[MPU_idx][2]);
+void printSensValues(int MPU_idx) {
+    printSensIdx("ax", MPU_idx);
+    Serial.print(acceleration[MPU_idx][0]);
+    printSensIdx("ay", MPU_idx);
+    Serial.print(acceleration[MPU_idx][1]);
+    printSensIdx("ax", MPU_idx);
+    Serial.print(acceleration[MPU_idx][2]);
 }
 
-void printSensIdx(char *varName, int MPU_idx)
-{
-  Serial.print(" ");
-  Serial.print(varName);
-  Serial.print(MPU_idx);
-  Serial.print(" ");
+void printSensIdx(char *varName, int MPU_idx) {
+    Serial.print(" ");
+    Serial.print(varName);
+    Serial.print(MPU_idx);
+    Serial.print(" ");
 }
