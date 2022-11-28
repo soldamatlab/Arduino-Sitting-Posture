35ff6425d894680bfb097f68d0a8b04797ddcaf4
Jonas Fer <ferjonas@fel.cvut.cz>
2022-11-28 15:31:18 +0100

    Refactor averaging

--- averaging/averaging.ino
+++ averaging/averaging.ino
@@ -1,59 +1,24 @@
 /*
-* Definice globalnich promennych
+* Howto: do mainu
+// global
+  bool position_ok = true;
+  
+  iteration(); // Changes values in the stack of accelerations.
+  position_ok = check_position(); // Checks all axes.
 */
-// Pole zrychleni
-// [osy] = [3] (xyz), [hodnoty] = [20] (hodnoty za 5s merene jednou za 250ms)
-// last_value je ukazatel na pole
-float g[3][20];
-int	last_value = 0;
 
-// [osy] = [3] (xyz), [hodnoty] = [2] (spdni mez a horni mez)
-float thresholds[3][2];
+// Global vars
+float g[3][20]; // [osy] = [3] (xyz), [hodnoty] = [20] (hodnoty za 5s merene jednou za 250ms 20k/250)
+int	last_value = 0; // last_value je ukazatel na pole
+float thresholds[3][2]; // [osy] = [3] (xyz), [hodnoty] = [2] (spdni mez a horni mez)
+//end globalni promenne
 
-// zrychleni v x,y,z
-float gx = 0, gy = 0, gz = 0;
-
-// checker spravne pozice
-bool position_ok = true;
-
+// Funkce:
 float average(int axis);
 bool check_position();
-  
-void setup() {
-  // Nastavit thresholdy pro porovnani - vyzkousime
-  thresholds[0][0] = -15.;
-  thresholds[0][1] = 15.;
-  thresholds[1][0] = -5.;
-  thresholds[1][1] = 5.;
-  thresholds[2][0] = -2.;
-  thresholds[2][1] = 2.;
-}
-
-void loop() {
-  // nejaka random iterace gcek
-	gx += 0.000001;
-	gy += 0.000005;
-  gz -= 0.000005;
-
-  // takhle se prepisuje pole   
-	g[0][last_value] = gx;
-	g[1][last_value] = gy;
-	g[2][last_value] = gz;
-	last_value++;
-  // pokud lastvalue dojde na konec, skocim s nim na prvni pozici
-	if (last_value == 20) {
-	  last_value = 0;
-	}
-  
-  // kontrola vsech os, jestli splnuji nejaky thrs
-  position_ok = check_position();  
-  if(!position_ok){
-    printf("Change position!");
-  }
-
-  delay(250);  
-
-}
+void init_thresholds();
+void iteration();
+//end funkce
 
 float average(int axis) {
 /*
@@ -68,23 +33,54 @@ float average(int axis) {
 
 bool check_position() {
 /*
-* Funkce projde vsechny radky pole a zkontroluje jestli prumer kazdeho radku lezi v thrs.
-* Pokud prumer nejakeho radku lezi mimo, vrati [0,1,2] podle osy, ktera je spatne [x,y,z].
-* 	+ chybove hlaseni
-* Pokud vsechny prumery odpovidaji thrs, vrati -1.
+* Function iterates through all rows of accelerations.
+* If any average lies outside of thrs, returns false.
+* 	+ Outputs Error message.
+* If all averages are accepted by thresholding, returns true.
 */
 	char axes[4] = "xyz";
 	for (int i = 0; i < 3; i++) {
 		float curr_av = average(i);
-		//cout << curr_av << " ";
-
+		
+    Serial.println();
 		if (curr_av > thresholds[i][0] && curr_av < thresholds[i][1]) {
-			//cout << "Position on " << axes[i] << " OK!\n";
+			Serial.print("Position on axis ");
+      Serial.print(axes[i]);
+      Serial.print("OK!");       
 		}
 		else {
-			//cout << "Change position on " << axes[i] << " axis!\n";
+			Serial.print("Change position on ");
+      Serial.print(axes[i]);
+      Serial.print(" axis!");
 			return false;
 		}
 	}
 	return true;
+}
+
+void init_thresholds(){
+/*
+* Function links thresholds from const.h to global var thresholds used in function check positions.
+*/  
+Prepsat tyhle random hodnoty na nejake realne
+  thresholds[0][0] = -15.;
+  thresholds[0][1] = 15.;
+  thresholds[1][0] = -5.;
+  thresholds[1][1] = 5.;
+  thresholds[2][0] = -2.;
+  thresholds[2][1] = 2.;  
+}
+
+void iteration(){
+/*
+* Function rewrites values in stack of iterations.
+*/
+Prepsat gx, gy, gz na values ze senzoru.
+	g[0][last_value] = gx;
+	g[1][last_value] = gy;
+	g[2][last_value] = gz;
+	last_value++;
+  if (last_value == 20) {
+	  last_value = 0;
+	}  
 }
